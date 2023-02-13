2af0222d5d2df2eced4fe218248f2e7a811ff7ff
Mohamed elsheikh <mohamed.elsheikh@reno.systems>
2023-02-13 16:31:46 +0200

    updates by ME

--- home.php
+++ home.php
@@ -6,3 +6,10 @@
  * @package WordPress
  * @subpackage LeMans
  */
+
+
+$context         = Timber::context();
+$timber_post     = Timber::query_post();
+$context['post'] = $timber_post;
+
+Timber::render('home.twig', $context);
