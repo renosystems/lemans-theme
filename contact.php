0ea871d44a1b303972c2dab8e8f1efb61c7a58a7
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-02-14 16:12:00 +0200

    create a contact.php and contact-form.twig for the contact form

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Template Name: Contactpage
+ *
+ * @package WordPress
+ * @subpackage LeMans
+ */
+
+
+$context         = Timber::context();
+$timber_post     = Timber::query_post();
+$context['post'] = $timber_post;
+
+//comment
+Timber::render('contact-form.twig', $context);
