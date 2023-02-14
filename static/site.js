914e3f5795879c91ebfd6de174b0170d4d441d5b
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-02-14 18:01:31 +0200

    add thank u feedback

--- static/site.js
+++ static/site.js
@@ -1,5 +1,17 @@
-jQuery( document ).ready( function( $ ) {
+jQuery(document).ready(function ($) {
+  let wpcf7Elm = document.querySelector(".wpcf7");
 
-  // Your JavaScript goes here
+  wpcf7Elm.addEventListener(
+    "wpcf7submit",
+    function (event) {
+      const eventDetails = event.detail
 
-});
+      if(eventDetails.status === "mail_sent"){
+        $("#thanku-modal-toggler").click();
+        const successMessage = document.querySelector(".wpcf7-response-output")
+        successMessage.classList.add("d-none")
+      }
+    },
+    false
+  )
+})
