4f9c96f1640192310151e02905d0dd4ed57d68f6
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-02-15 17:44:53 +0200

    add featured and available now posts for home page

--- home.php
+++ home.php
@@ -8,9 +8,60 @@
  */
 
 
-$context         = Timber::context();
-$timber_post     = Timber::query_post();
+$context = Timber::context();
+$timber_post = Timber::query_post();
 $context['post'] = $timber_post;
+
+$context['carsAvailableNow'] = new Timber\PostQuery([
+  'post_type' => 'cars',
+  'meta_query' => array(
+    'relation' => 'AND',
+    array(
+      'key' => 'featured',
+      'value' => 1,
+    ),
+    array(
+      'key' => 'available_now',
+      'value' => 1,
+    )
+  )
+  //'meta_key' => 'featured',
+  //'meta_value' => 1,
+  //'numberposts' => 1,
+  //'tax_query' => [
+  //    [
+  //        'taxonomy' => 'car-type',
+  //        'field' => 'slug',
+  //        'terms' => 'sports-car',
+  //    ],
+  //],
+]);
+
+$context['featuredCars'] = new Timber\PostQuery([
+  'post_type' => 'cars',
+    'numberposts' => 3,
+  'meta_query' => array(
+    'relation' => 'AND',
+    array(
+      'key' => 'featured',
+      'value' => 1,
+    ),
+    array(
+      'key' => 'available_now',
+      'value' => 0,
+    )
+  )
+]);
+
+$context['sportsCars'] = new Timber\PostQuery([
+  'tax_query' => [
+      [
+          'taxonomy' => 'car-type',
+          'field' => 'slug',
+          'terms' => 'sports-car',
+      ],
+  ],
+]);
 
 //comment
 Timber::render('home.twig', $context);
