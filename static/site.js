34c5078b16ae7018ce8a08e54862a46903f508d7
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-02-19 16:25:40 +0200

    update content

--- static/site.js
+++ static/site.js
@@ -1,17 +1,19 @@
 jQuery(document).ready(function ($) {
   let wpcf7Elm = document.querySelector(".wpcf7");
+  if (wpcf7Elm)
+    wpcf7Elm.addEventListener(
+      "wpcf7submit",
+      function (event) {
+        const eventDetails = event.detail;
 
-  wpcf7Elm.addEventListener(
-    "wpcf7submit",
-    function (event) {
-      const eventDetails = event.detail
-
-      if(eventDetails.status === "mail_sent"){
-        $("#thanku-modal-toggler").click();
-        const successMessage = document.querySelector(".wpcf7-response-output")
-        successMessage.classList.add("d-none")
-      }
-    },
-    false
-  )
-})
+        if (eventDetails.status === "mail_sent") {
+          $("#thanku-modal-toggler").click();
+          const successMessage = document.querySelector(
+            ".wpcf7-response-output"
+          );
+          successMessage.classList.add("d-none");
+        }
+      },
+      false
+    );
+});
