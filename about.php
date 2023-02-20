dbb65f9d1f33b7c3da1eb8d3576e907f92171da1
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-02-20 12:58:26 +0200

    add about page

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Template Name: AboutPage
+ *
+ * @package WordPress
+ * @subpackage LeMans
+ */
+
+
+$context         = Timber::context();
+$timber_post     = Timber::query_post();
+$context['post'] = $timber_post;
+
+//comment
+Timber::render('page-about.twig', $context);
