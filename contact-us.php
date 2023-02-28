4a17c50d306b35c0e0b7b0e2289d4f4d61b7fb21
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-02-28 15:21:21 +0200

    show requested car images and name in the contact form using query vars and update styles

--- contact-us.php
+++ contact-us.php
@@ -8,10 +8,21 @@
  */
 
 
-$context         = Timber::context();
-$timber_post     = Timber::query_post();
+$context = Timber::context();
+$timber_post = Timber::query_post();
 $context['post'] = $timber_post;
 $context['options'] = get_fields('options');
+
+$queryAbout = sanitize_text_field(get_query_var('queryAbout'));
+
+if ($queryAbout) {
+  $context['queryAbout'] = $queryAbout;
+  $context['cars'] = new Timber\PostQuery([
+    'post_type' => 'cars',
+    'post_name__in' => [$queryAbout],
+  ]);
+
+}
 
 //comment
 Timber::render('page-contact-us.twig', $context);
