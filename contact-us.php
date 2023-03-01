33d721aa2d782ace49e3c270630597a735a8466f
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-03-01 16:17:42 +0200

    remove cars logic from contact-us

--- contact-us.php
+++ contact-us.php
@@ -13,16 +13,5 @@ $timber_post = Timber::query_post();
 $context['post'] = $timber_post;
 $context['options'] = get_fields('options');
 
-$queryAbout = sanitize_text_field(get_query_var('queryAbout'));
-
-if ($queryAbout) {
-  $context['queryAbout'] = $queryAbout;
-  $context['cars'] = new Timber\PostQuery([
-    'post_type' => 'cars',
-    'post_name__in' => [$queryAbout],
-  ]);
-
-}
-
 //comment
 Timber::render('page-contact-us.twig', $context);
