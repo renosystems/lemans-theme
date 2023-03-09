d8fb41acca0b6a382c90fd99c008e69f1ae5b55d
ShehabAhmed97 <shehabkhaleel@gmail.com>
2023-03-09 12:11:01 +0200

    car can be in featured and available cars at the same time

--- home.php
+++ home.php
@@ -14,35 +14,38 @@ $context['post'] = $timber_post;
 
 $context['carsAvailableNow'] = new Timber\PostQuery([
   'post_type' => 'cars',
-  'meta_query' => array(
-    'relation' => 'AND',
-    array(
-      'key' => 'featured',
-      'value' => 1,
-    ),
-    array(
-      'key' => 'available_now',
-      'value' => 1,
-    )
-  )
-  //'meta_key' => 'featured',
-  //'meta_value' => 1,
+  //'meta_query' => array(
+  //  'relation' => 'AND',
+  //  array(
+  //    'key' => 'featured',
+  //    'value' => 1,
+  //  ),
+  //  array(
+  //    'key' => 'available_now',
+  //    'value' => 1,
+  //  )
+  //)
+  'meta_key' => 'available_now',
+  'meta_value' => 1,
 ]);
 
 $context['featuredCars'] = new Timber\PostQuery([
   'post_type' => 'cars',
   'numberposts' => 3,
-  'meta_query' => array(
-    'relation' => 'AND',
-    array(
-      'key' => 'featured',
-      'value' => 1,
-    ),
-    array(
-      'key' => 'available_now',
-      'value' => 0,
-    )
-  )
+  //'meta_query' => array(
+  //  'relation' => 'AND',
+  //  array(
+  //    'key' => 'featured',
+  //    'value' => 1,
+  //  ),
+  //  array(
+  //    'key' => 'available_now',
+  //    'value' => 0,
+  //  )
+  //)
+  'meta_key' => 'featured',
+  'meta_value' => 1,
+
 ]);
 
 $context['cartypes'] = Timber::get_terms([
